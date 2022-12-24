bb1c0658f737226014b76566a4168fdff09991e6
jannik-reaZn <64127899+jannik-reaZn@users.noreply.github.com>
2022-12-24 11:44:32 +0100

    Add files via upload

---
+++ keys.py
@@ -0,0 +1,11 @@
+keys = {
+  'Lebensmittel': ['Budni', 'Penny', 'Rewe', 'Aldi', 'Lidl', 'Edeka', 'DM', 'Rossmann', 'Netto', 'Getränke Hoffmann', 'Getränkeshop Höpner', 'Biomarkt', 'Tchibo'],
+  'Kleidung': ['Peek und Cloppenburg', 'Galeria Kaufhof', 'H+M', 'C&A', '1000 Ernstings Family', 'Babyone', 'Deichmann', 'Schuhkay', 'Hollister', 'Vera Moda', 'Strick und Stick', 'Police Herrenkleidung', 'Sportscheck'],
+  'Sport': ['Niendorfer Turn und Sportverein', 'TUS Germania Schnelsen', 'Birgit Hansen', 'SV Eidelstedt'],
+  'Auto': ['Star', 'Aral', 'HEM', 'Total', 'HUK', 'Auto Wichert'],
+  'Wohnung': ['Thor', 'Wilhelmtel', 'Vattenfall', 'Lekker Energie', 'Norddeutscher Rundfunk', 'Mobilcom', 'Dazn', 'TV Spielfilm'],
+  'Vorsorge': ['Nürnberger LV', 'Signal Iduna', 'Bayerische Beamtenkrankenhaus'],
+  'Sonstige 1': ['Hartfelder', 'Smithstoys', 'My Toys', 'Thalia', 'Staples'],
+  'Sonstige 2': ['Media-Saturn', 'Media Markt', 'Amazon'],
+  'Sonstige 3': ['Ikea', 'Möbel Höffner', 'Möbel Schulenberg', 'Bauhaus', 'Hornbach', 'Fielmann', 'Blume 2000', 'Gries Deko']
+}
